1cfac4c8bdab43719991722cb82cf67540b0e452
luvvbladess <151755830+luvvbladess@users.noreply.github.com>
2025-06-23 13:40:01 +0300

    update

---
+++ app.py
@@ -0,0 +1,16 @@
+   #!/usr/bin/env python3
+   import os
+   from flask import Flask, request, escape
+
+   app = Flask(__name__)
+
+   @app.route("/", methods=["GET"])
+   def root():
+       name    = request.args.get("name",    "Recruto")
+       message = request.args.get("message", "Давай дружить")
+       # Экранируем, чтобы избежать XSS при произвольных значениях
+       return f"Hello {escape(name)}! {escape(message)}"
+
+   if __name__ == "__main__":
+       port = int(os.getenv("PORT", 8000))
+       app.run(host="0.0.0.0", port=port)
